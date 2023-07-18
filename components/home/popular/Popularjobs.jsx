287691599c5c147ce6860a7069e8b0a2adeeb7b4
Pablo Picasso Sancho <cicada3319@gmail.com>
2023-07-18 12:56:39 +0300

    job-cards

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,12 +6,50 @@ import { View, Text, TouchableOpacity, FlatList, ActivityIndicator}
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from "../../common/cards/popular/PopularJobCard"
+import useFetch from '../../../hook/useFetch'
+
 
 const Popularjobs = () => {
   const router = useRouter()
+  const { data, isLoading, error } = useFetch("search", {
+      query: "React developer",
+      num_pages: "1"
+  });
+
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show  All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}> 
+        { isLoading ? (
+        <ActivityIndicator size="large" colors={COLORS.primary}/>
+        ): error ?  (
+          <Text>{error.message}</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({item}) => (
+                <PopularJobCard
+                  item={item}
+                />
+            )}
+            keyExtractor={ item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+             >
+
+          </FlatList>
+        ) }
+
+
+      </View>
+
     </View>
   )
 }
